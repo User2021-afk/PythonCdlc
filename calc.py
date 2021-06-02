f3e48f667060a7c5a08a90a2c82e6332a32117d1
Dimas <dima.sikora.97@mail.ru>
2021-06-02 17:33:16 +0300

    Add calc.py

---
+++ calc.py
@@ -0,0 +1,11 @@
+# программа на пaйтоне калькулятор 1.0
+print("Привет! Это ваш калькулятор!")
+print("Ведите первое число")
+print("Видие знак")
+print("Видите второе число")
+print("Нажмите энтер")
+first_NUM = float(input())
+operator = str(lhpvt())
+prlnt("2-e число")
+second_num = float(input())
+
